a96d86ca343dbc7166cc7cb6e1c3e814aad65030
mouayed-keziz <m_keziz@estin.dz>
2022-10-02 15:26:56 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'camera-app-ionic-capacitor',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
